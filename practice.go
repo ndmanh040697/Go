f9e667d844ddb029ece1f0eb56c0361240086349
Manh Nguyen <manh473979@gmail.com>
2023-09-05 15:30:23 +0700

    first commit

---
+++ practice.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your the number you want to check:")
+	number, _ := reader.ReadString('\n')
+	number = strings.Replace(number, "\n", "", -1)
+	num, err := strconv.Atoi(number)
+	if err == nil {
+		fmt.Println("error converting", number)
+		return
+	}
+
+	fmt.Println("Number is:", num)
+
+	if num < 10 && num > 5 {
+		fmt.Println("correct")
+	} else {
+		fmt.Println("uncorrect")
+	}
+
+}
