cdc4b2b441899fc0a9f57e1157df1563b739f2f2
Manh Nguyen <manh473979@gmail.com>
2023-09-07 15:03:05 +0700

    updated

--- sortNumber.go
+++ sortNumber.go
@@ -5,25 +5,36 @@ import (
 )
 
 func main() {
-	slice := []int{}
-	for {
-		var input int
-		fmt.Print("Input Number=")
-		fmt.Scan(&input)
-		slice = append(slice, input)
+	// slice := []int{}
+	// for {
+	// 	var input int
+	// 	fmt.Print("Input Number=")
+	// 	fmt.Scan(&input)
+	// 	slice = append(slice, input)
 
-		if input == 100 {
-			break
-		}
-		fmt.Println(bubbleSort(slice))
-	}
+	// 	if input == 100 {
 
-	// a := []int{1, 4, 7, 2, 3}
-	// fmt.Println(bubbleSort(a))
+	// 		var confirm string
+	// 		fmt.Println("Would you want to stop ? y/n:")
+	// 		fmt.Scan(&confirm)
+
+	// 		if confirm == "y" {
+	// 			break
+	// 		} else if confirm == "n" {
+	// 			continue
+	// 		}
+
+	// 	}
+	// }
+	// fmt.Println(bubbleSort(slice))
+
+	a := []int{1, 4, 7, 2, 3, 8, 5, 9, 6}
+	fmt.Println(bubbleSort(a))
+	fmt.Println(mergeSort(a))
 }
 
 func bubbleSort(a []int) []int {
-	for i := 0; i < (len(a) - 1); i++ {
+	for i := 0; i < (len(a) - 2); i++ {
 		for j := 0; j < len(a)-1; j++ {
 			if a[j] < a[j+1] {
 				a[j], a[j+1] = a[j+1], a[j]
@@ -33,4 +44,34 @@ func bubbleSort(a []int) []int {
 
 	}
 	return a
+}
+
+func mergeSort(a []int) []int {
+	if len(a) <= 1 {
+		return a
+	}
+	mid := len(a) / 2
+	left := a[:mid]
+	right := a[mid:]
+	return merge(left, right)
+}
+func merge(left, right []int) []int {
+	result := make([]int, len(left)+len(right))
+	i, j := 0, 0
+	for k := 0; k < len(result); k++ {
+		if i >= len(left) {
+			result[k] = right[j]
+			j++
+		} else if j >= len(right) {
+			result[k] = left[i]
+			i++
+		} else if left[i] < right[j] {
+			result[k] = left[i]
+			i++
+		} else {
+			result[k] = right[j]
+			j++
+		}
+	}
+	return result
 }
